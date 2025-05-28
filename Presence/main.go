1d279621e9d06094bc7ad46b68eaf60335fde88f
BenPfeiffer-TX <ben.pfeiffer97@gmail.com>
2025-05-28 11:03:57 -0500

    implemented putStatus() to clean up code, also added color display and status selection in index.html

--- Presence/main.go
+++ Presence/main.go
@@ -33,6 +33,22 @@ func getStatus() []StatusMap {
 	return statusArray
 }
 
+func putStatus(newArray []StatusMap) error {
+	file, err := os.OpenFile("status.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		fmt.Println("failed to open/truncate .json")
+		return err
+	}
+	defer file.Close()
+	//encode new data to status.json
+	err = json.NewEncoder(file).Encode(newArray)
+	if err != nil {
+		fmt.Println("failed to encode to .json")
+		return err
+	}
+	return nil
+}
+
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	statusArr := getStatus()
 	data := map[string][]StatusMap{"StatusMap": statusArr}
@@ -60,26 +76,17 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	//read the contents of status.json
 	statusArray := getStatus()
 	statusArray = append(statusArray, newEntry)
-	file, err := os.OpenFile("status.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+	err := putStatus(statusArray)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
 	}
-	defer file.Close()
-	//encode new data to status.json
-	err = json.NewEncoder(file).Encode(statusArray)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	//display delete.html, which displays boxes of usernames and, when clicked, they are removed from list
-	statusArr := getStatus()
-	data := map[string][]StatusMap{"StatusMap": statusArr}
+	statusArray := getStatus()
+	data := map[string][]StatusMap{"StatusMap": statusArray}
 	err := templates.ExecuteTemplate(w, "delete.html", data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -87,15 +94,32 @@ func deleteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func removeHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.FormValue("name")
-	statusArray := getStatus()
+	//name := r.FormValue("name")
+	//statusArray := getStatus()
 	//...
 
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func updateHandler(w http.ResponseWriter, r *http.Request) {
-
+	//we receive a statusmap struct from the javascript in index.html
+	var newEntry StatusMap
+	err := json.NewDecoder(r.Body).Decode(&newEntry)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	//we load the current statusArray and iterate through it until we match on the user we are updating
+	currentArray := getStatus()
+	for _, user := range currentArray {
+		if user.Name == newEntry.Name {
+			user.Status = newEntry.Status
+		}
+	}
+	//push updated array content to json
+	err = putStatus(currentArray)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func main() {
